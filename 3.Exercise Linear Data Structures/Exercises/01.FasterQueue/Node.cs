fda223f0a19a8d8c263749c3d04b74056de2dd6d
evgeni136 <evgeni_136@abv.bg>
2023-01-02 21:11:07 +0200

    Solved Balanced Parentheses task

--- 3.Exercise Linear Data Structures/Exercises/01.FasterQueue/Node.cs
+++ 3.Exercise Linear Data Structures/Exercises/01.FasterQueue/Node.cs
@@ -7,14 +7,17 @@
 
         }
 
-        public Node(T item, Node<T> next)
+        public Node(T item, Node<T> next = null, Node<T> previous = null)
         {
             this.Item = item;
             this.Next = next;
+            this.Previous = previous;
         }
 
         public T Item { get; set; }
 
         public Node<T> Next { get; set; }
+
+        public Node<T> Previous { get; set; }
     }
 }
---
+++ 3.Exercise Linear Data Structures/Exercises/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,58 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            var parenthesesArray = parentheses.ToCharArray();
+            var parenthesesStack = new Stack<char>();
+
+            foreach (var bracket in parenthesesArray)
+            {
+                if (bracket == '(' ||
+                    bracket == '{' ||
+                    bracket == '[')
+                {
+                    parenthesesStack.Push(bracket);
+                    continue;
+                }
+
+                if (parenthesesStack.Count > 0)
+                {
+                    var lastBracket = parenthesesStack.Pop();
+
+                    if (bracket == ')')
+                    {
+                        if (lastBracket != '(')
+                        {
+                            return false;
+                        }
+                    }
+                    else if (bracket == '}')
+                    {
+                        if (lastBracket != '{')
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if (lastBracket != '[')
+                        {
+                            return false;
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
