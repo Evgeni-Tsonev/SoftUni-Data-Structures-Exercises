ff432a08e9fed7bea8b15dbe50074c3b807bb0f6
evgeni136 <evgeni_136@abv.bg>
2023-01-02 17:24:45 +0200

    Implemented DoubleLinkedList

---
+++ 3.Exercise Linear Data Structures/Exercises/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,119 @@
+﻿namespace Problem02.DoublyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class DoublyLinkedList<T> : IAbstractLinkedList<T>
+    {
+        private Node<T> head;
+
+        private Node<T> tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T item)
+        {
+            var newNode = new Node<T>(item);
+
+            if (this.Count == 0)
+            {
+                this.head = newNode;
+                this.tail = newNode;
+            }
+            else
+            {
+                newNode.Next = this.head;
+                this.head.Previous = newNode;
+                this.head = newNode;
+            }
+
+            this.Count++;
+        }
+
+        public void AddLast(T item)
+        {
+            var newNode = new Node<T>(item);
+
+            if (this.Count == 0)
+            {
+                this.head = newNode;
+                this.tail = newNode;
+            }
+            else
+            {
+                newNode.Previous = this.tail;
+                this.tail.Next = newNode;
+                this.tail = newNode;
+            }
+
+            this.Count++;
+        }
+
+        public T GetFirst()
+        {
+            this.EnsureNotEmpty();
+
+            return this.head.Item;
+        }
+
+        public T GetLast()
+        {
+            this.EnsureNotEmpty();
+
+            return this.tail.Item;
+        }
+
+        public T RemoveFirst()
+        {
+            this.EnsureNotEmpty();
+
+            var itemToReturn = this.head.Item;
+            var newHead = this.head.Next;
+            this.head.Next = null;
+            this.head = newHead;
+
+            this.Count--;
+
+            return itemToReturn;
+        }
+
+        public T RemoveLast()
+        {
+            this.EnsureNotEmpty();
+
+            var itemToReturn = this.tail.Item;
+            var newTail = this.tail.Previous;
+            this.tail.Previous = null;
+            if (newTail != null)
+            {
+                newTail.Next = null;
+            }
+
+            this.tail = newTail;
+            this.Count--;
+
+            return itemToReturn;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = this.head;
+
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Count == 0)
+                throw new InvalidOperationException();
+        }
+    }
+}
