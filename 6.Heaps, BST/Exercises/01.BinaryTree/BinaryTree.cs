d4b7dad4ed01dae6bbad4eba070e564343937eac
evgeni136 <evgeni_136@abv.bg>
2023-01-10 18:37:18 +0200

    Implemented AsIndentedPreOrder method

---
+++ 6.Heaps, BST/Exercises/01.BinaryTree/BinaryTree.cs
@@ -0,0 +1,69 @@
+﻿namespace _01.BinaryTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class BinaryTree<T> : IAbstractBinaryTree<T>
+    {
+        public BinaryTree(T element, IAbstractBinaryTree<T> left, IAbstractBinaryTree<T> right)
+        {
+            this.Value = element;
+            this.LeftChild = left;
+            this.RightChild = right;
+        }
+
+        public T Value { get; set; }
+
+        public IAbstractBinaryTree<T> LeftChild { get; set; }
+
+        public IAbstractBinaryTree<T> RightChild { get; set; }
+
+        public string AsIndentedPreOrder(int indent)
+        {
+            var sb = new StringBuilder();
+
+            var result = this.Dfs(this, sb, indent);
+
+            return result;
+        }
+
+        private string Dfs(IAbstractBinaryTree<T> tree, StringBuilder sb, int indent)
+        {
+            sb.Append(new string(' ', indent));
+            sb.AppendLine(tree.Value.ToString());
+
+            if (tree.LeftChild != null)
+            {
+                this.Dfs(tree.LeftChild, sb, indent + 2);
+            }
+
+            if (tree.RightChild != null)
+            {
+                this.Dfs(tree.RightChild, sb, indent + 2);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public void ForEachInOrder(Action<T> action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> InOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> PostOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IAbstractBinaryTree<T>> PreOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 6.Heaps, BST/Exercises/Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Demo
+{
+    using System;
+    using _01.BinaryTree;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new BinaryTree<int>(
+                          17,
+                          new BinaryTree<int>(
+                              9,
+                              new BinaryTree<int>(3, null, null),
+                              new BinaryTree<int>(11, null, null)
+                          ),
+                          new BinaryTree<int>(
+                              25,
+                              new BinaryTree<int>(20, null, null),
+                              new BinaryTree<int>(31, null, null)));
+
+            var result = tree.AsIndentedPreOrder(0);
+
+            Console.WriteLine(result);
+        }
+    }
+}
